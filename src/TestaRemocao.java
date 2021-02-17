0309fa9e23156f6bd2bb6e51a6025840784100b5
Leonardo Rocha Andreazzi <zarp_landreazzi@orizon.com.br>
2021-02-17 18:16:29 -0300

    deletando itens em uma determinada tabela

--- src/TestaRemocao.java
+++ src/TestaRemocao.java
@@ -1,4 +1,22 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestaRemocao {
 
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.createConnection();
+		
+		Statement statement = connection.createStatement();
+		
+		statement.execute("DELETE FROM produto WHERE id > 2");
+		
+		int count = statement.getUpdateCount();
+		
+		System.out.println(count);
+		
+		connection.close();
+	}
 }
