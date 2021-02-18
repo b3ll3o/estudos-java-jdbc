502f6b248278e2329cccd1a93dea5e7d0016bd32
Leonardo Rocha Andreazzi <zarp_landreazzi@orizon.com.br>
2021-02-18 03:55:57 -0300

    brincando com pool de connections

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,10 +1,25 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.PooledConnection;
+
+import org.postgresql.ds.PGConnectionPoolDataSource;
 
 public class ConnectionFactory {
 
+	private PooledConnection dataSource;
+	
+	public ConnectionFactory() throws SQLException {
+		PGConnectionPoolDataSource dataSource = new PGConnectionPoolDataSource();
+		
+		dataSource.setURL("jdbc:postgresql://localhost/java_jdbc");
+		dataSource.setUser("postgres");
+		dataSource.setPassword("123");
+		
+		this.dataSource = dataSource.getPooledConnection();
+	}
+	
 	public Connection createConnection() throws SQLException {
-		return DriverManager.getConnection("jdbc:postgresql://localhost/java_jdbc", "postgres", "123");
+		return this.dataSource.getConnection();
 	}
 }
--- src/TestaListagem.java
+++ src/TestaListagem.java
@@ -7,27 +7,22 @@ public class TestaListagem {
 
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		
-		Connection connection = connectionFactory.createConnection();
-		
-		PreparedStatement statement = connection.prepareStatement("select * from produto");
-		
-		boolean execute = statement.execute();
-		
-		System.out.println(execute);
-		
-		ResultSet resultSet = statement.getResultSet();
-		
-		while(resultSet.next()) {
-			Integer id = resultSet.getInt("id");
-			String nome = resultSet.getString("nome");
-			String descricao = resultSet.getString("descricao");
-			
-			System.out.println("Id: " + id + ", Nome: " + nome + ", Descrição: " + descricao); 
+		try(Connection connection = connectionFactory.createConnection()){
+
+			try(PreparedStatement statement = connection.prepareStatement("select * from produto")){
+				statement.execute();
+								
+				try(ResultSet resultSet = statement.getResultSet()){
+					
+					while(resultSet.next()) {
+						Integer id = resultSet.getInt("id");
+						String nome = resultSet.getString("nome");
+						String descricao = resultSet.getString("descricao");
+						
+						System.out.println("Id: " + id + ", Nome: " + nome + ", Descrição: " + descricao); 
+					}
+				}				
+			}	
 		}
-		
-		statement.close();
-		
-		connection.close();
 	}
 }
